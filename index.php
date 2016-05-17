138e70cd9aa0dcf742314ba84b86a0c548554605
ljx8948 <ljx8948@outlook.com>
2016-05-17 12:55:32 +0000

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Hello world!</title>
+	<style>
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Helvetica Neue",Helvetica,"Microsoft YaHei",Arial,"宋体","Hiragino Sans GB",STHeiti,"WenQuanYi Micro Hei",SimSun,sans-serif;
+	}
+
+	#logo {
+		margin-bottom: 40px;
+		width: 300px;
+	}
+	</style>
+</head>
+<body>
+	<img id="logo" src="logo.png" />
+	<h1><?php echo "Hello world!"; ?></h1>
+	<h2><?php echo "PHP runtime image, include Ubuntu, Apache, PHP5, except Mysql database"; ?></h2>
+</body>
+</html>
